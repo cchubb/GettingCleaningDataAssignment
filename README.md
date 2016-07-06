d3b743a8760c9822b3061a0f82eda2b013b2d5b9
Chris Chubb <cchubb@codegurus.com>
2016-07-06 12:34:19 -0400

    Removed the Inertial Signals data processing. Started on documentation too.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# GettingCleaningDataAssignment
-For the class assignment
+Getting and Cleaning Data Course Project
+========================================
+Chris Chubb
+cchubb@codegurus.com
+Create Date: July 6, 2016
+========================================
+
+
+### Requirements:
+
+  * Merge the training and the test sets to create one data set.
+  * Extract only the measurements on the mean and standard deviation for each measurement.
+  * Use descriptive activity names to name the activities in the data set
+  * Appropriately label the data set with descriptive variable names.
+  * From the data set in step 4, create a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## Data Description
+
+The source data consists of accelerometer data collected from smartphones worn by test subjects performing a variety of actions. 
+
+### Synopsis:
+
+---
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+---
+
+Additional details can be found at the [Human Activity Recognition Using Smartphones Data Set] (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+The data is downloaded from the [compressed archive](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+## Process
+
+The included script, run_analysis.R, performs the following steps on the provided data in order to reformat and summarize the data into a tidy data set for further analysis.
+
--- run_analysis.R
+++ run_analysis.R
@@ -8,10 +8,12 @@
 #Appropriately labels the data set with descriptive variable names.
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-#Ensure I am working in the right project directory.
+#Ensure I am working in the right project directory. Uncomment only on my own system. Evaluators will need to set this themselves.
 setwd("C:\\Users\\cchubb\\Coursera\\DataScience\\3Getting and Cleaning Data\\GettingCleaningDataAssignment")
 #Clear out any local variables, start fresh and remove unneeded data from the global environment
 rm(list = ls())
+
+library(dplyr)
 
 #Connect to the server and download the file if it does not already exist
 if (! file.exists("./data")) { dir.create("./data")}
@@ -32,94 +34,34 @@ if (! file.exists("./data/UCI HAR Dataset/README.txt")) {
 data_dir <- "./data/UCI HAR Dataset"
 
 #Get activity labels
-activity_labels <- read.table(paste(data_dir, "/activity_labels.txt", sep=""))
+activity_labels <- read.table(paste0(data_dir, "/activity_labels.txt"))
 colnames(activity_labels) <- c("activity_id", "activity_label")
 
 #Get feature labels
-feature_labels <- read.table(paste(data_dir, "/features.txt", sep=""))
+feature_labels <- read.table(paste0(data_dir, "/features.txt"))
 colnames(feature_labels) <- c("feature_id", "feature_label")
 
 #Read and union test and train data together.
 subject_merge  <- rbind(
-  read.table(paste(data_dir, "/test/subject_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/subject_train.txt", sep=""))
+  read.table(paste0(data_dir, "/test/subject_test.txt")), 
+  read.table(paste0(data_dir, "/train/subject_train.txt"))
 )
 y_merge  <- rbind(
-  read.table(paste(data_dir, "/test/y_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/y_train.txt", sep=""))
+  read.table(paste0(data_dir, "/test/y_test.txt")), 
+  read.table(paste0(data_dir, "/train/y_train.txt"))
 )
 
 
-body_acc_x_merge  <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/body_acc_x_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/body_acc_x_train.txt", sep=""))
-)
-body_acc_y_merge  <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/body_acc_y_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/body_acc_y_train.txt", sep=""))
-)  
-body_acc_z_merge  <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/body_acc_z_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/body_acc_z_train.txt", sep=""))
-)  
-body_gyro_x_merge <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/body_gyro_x_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/body_gyro_x_train.txt", sep=""))
-) 
-body_gyro_y_merge <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/body_gyro_y_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/body_gyro_y_train.txt", sep=""))
-) 
-body_gyro_z_merge <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/body_gyro_z_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/body_gyro_z_train.txt", sep=""))
-) 
-total_acc_x_merge <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/total_acc_x_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/total_acc_x_train.txt", sep=""))
-) 
-total_acc_y_merge <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/total_acc_y_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/total_acc_y_train.txt", sep=""))
-) 
-total_acc_z_merge <- rbind(
-  read.table(paste(data_dir, "/test/Inertial Signals/total_acc_z_test.txt", sep="")), 
-  read.table(paste(data_dir, "/train/Inertial Signals/total_acc_z_train.txt", sep=""))
-)
-
-#Add the subject and activity IDs, merge in the activity labels, plus find the means and standard deviations for each set of data raw data
+#Add the subject and activity IDs, merge in the activity labels
 data_summary <-  merge(
   data.frame(
     subject = subject_merge$V1, 
-    activity_id = y_merge$V1, 
-    
-    body_acc_x_mean = rowMeans(body_acc_x_merge),
-    body_acc_x_stdev = apply(body_acc_x_merge, 1, sd),
-    
-    body_acc_y_mean = rowMeans(body_acc_y_merge),
-    body_acc_y_stdev = apply(body_acc_y_merge, 1, sd),
-    
-    body_acc_z_mean = rowMeans(body_acc_z_merge),
-    body_acc_z_stdev = apply(body_acc_z_merge, 1, sd),
-    
-    body_gyro_x_mean = rowMeans(body_gyro_x_merge),
-    body_gyro_x_stdev = apply(body_gyro_x_merge, 1, sd),
-    
-    body_gyro_y_mean = rowMeans(body_gyro_y_merge),
-    body_gyro_y_stdev = apply(body_gyro_y_merge, 1, sd),
-    
-    body_gyro_z_mean = rowMeans(body_gyro_z_merge),
-    body_gyro_z_stdev = apply(body_gyro_z_merge, 1, sd),
-    
-    total_acc_x_mean = rowMeans(total_acc_x_merge),
-    total_acc_x_stdev = apply(total_acc_x_merge, 1, sd),
-    
-    total_acc_y_mean = rowMeans(total_acc_y_merge),
-    total_acc_y_stdev = apply(total_acc_y_merge, 1, sd),
-    
-    total_acc_z_mean = rowMeans(total_acc_z_merge),
-    total_acc_z_stdev = apply(total_acc_z_merge, 1, sd)
-  ), activity_labels, by="activity_id")
+    activity_id = y_merge$V1
+  ), 
+  activity_labels, 
+  by="activity_id",
+  sort=FALSE
+  )
 
 #Read the X feature summary for test/train and merge together
 X_merge <- rbind(
@@ -132,17 +74,27 @@ colnames(X_merge) <- feature_labels$feature_label
 #X_merge[["subject"]] <- subject_merge$V1
 #X_merge[["activity_id"]] <- y_merge$V1
 
+#Normalize the labels to make them more "tidy".
+
+
 #Extract only the mean()/std() variables from the feature set for column appending to the real data
 data_summary <- cbind(
   data_summary,
   X_merge[, grepl("(mean\\()|(std\\()", names(X_merge))]
 )
 
+#Convert the subject integer column to a factor. 
+data_summary$subject <- as.factor(data_summary$subject)
+
+#I no longer need the activity_id column, that data is in the activity_label factor column:
+data_summary <- select(data_summary, -activity_id)
+
+
 
 #From the data set in step 4, creates a second, independent tidy data set 
 #  with the average of each variable for each activity and each subject.
 
-
+data_summary_grouped <- group_by(data_summary, subject, activity_label)
 
 
 
